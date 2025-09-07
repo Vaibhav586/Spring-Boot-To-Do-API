d0aeffc35b5437b1b700619a4ec51a5177eb3d0a
Vaibhav <111916301+Vaibhav586@users.noreply.github.com>
2025-09-07 18:51:51 +0530

    Create SpringbootTodoApiApplication.java

---
+++ SpringbootTodoApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTodoApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTodoApiApplication.class, args);
+    }
+}
